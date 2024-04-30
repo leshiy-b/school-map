b45fe21cfb4a489c0f6956794d6b99ee13619c5b
Michael Bogorad <bmikle@gmail.com>
2024-04-30 18:13:38 -0400

    Plan

---
+++ Plan.py
@@ -0,0 +1,6 @@
+"""
+1. Learn how to put an image to an HTML
+2. Learn how to make an html form which allows to enter something (room number)
+3. Learn how to show different images based on a room number.
+
+"""
